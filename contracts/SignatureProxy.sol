95853b1dd30c2c78c54e726939c4df21636deea6
Anna Wang <s067220898@gmail.com>
2016-08-22 10:59:34 -0400

    fixed replay attack vulnerability

--- contracts/SignatureProxy.sol
+++ contracts/SignatureProxy.sol
@@ -4,36 +4,45 @@
 **/
 
 contract SignatureProxy {
+    // Signer is the person whose transactions are being forwarded by this contract
     address signer;
     // Destination is the address of the contract that method calls are sent to
     address destination;
+    // Nonce is to keep from having replay attacks
+    uint nonce;
     
     // Ensures that the signer has signed the hash of the desired method call
      modifier onlySigner(
-        // hash is sha3("methodName(paramType,paraType...)")
-        bytes32 hash, 
+        // methodSignature is sha3("methodName(paramType,paraType...)")
+        bytes32 methodSignature, 
+        bytes32[] callData,
+        uint _nonce,
         uint8 v, 
         bytes32 r, 
         bytes32 s
     ) {
-            if (ecrecover(hash, v, r, s) != signer) throw;
+        if (_nonce <= nonce) throw;
+        nonce = _nonce;
+        bytes32 hash = sha3(methodSignature, callData, _nonce);
+        if (ecrecover(hash, v, r, s) != signer) throw;
         _
     }
     
     function SignatureProxy(address _signer, address _destination) {
         signer = _signer;
         destination = _destination;
+        nonce = 0;
     }
     
     // Returns true upon success and false upon failure
     function forward(
-        bytes32 hash, 
+        bytes32 methodSignature, 
+        bytes32[] callData,
         uint8 v, 
         bytes32 r, 
-        bytes32 s,
-        bytes32[] callData
+        bytes32 s
     )
-    onlySigner(hash, v, r, s) returns (bool) {
+    onlySigner(methodSignautre, callData, v, r, s) returns (bool) {
         return destination.call(bytes4(hash), callData);
     }
 }
