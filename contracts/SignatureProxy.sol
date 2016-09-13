92ee8d164334b710ddb50f93fe063b47f120dc79
Jeff Coleman <jeff@ledgerlabs.io>
2016-09-13 08:26:58 -0400

    fixed, made more general, made nonces per-call

--- contracts/SignatureProxy.sol
+++ contracts/SignatureProxy.sol
@@ -1,48 +1,47 @@
 /**
 * This contract will call some arbitrary contract's methods
-* if and only if the signer has signed off on the order to
+* if and only if the signer has signed off on the request
 **/
 
 contract SignatureProxy {
     // Signer is the person whose transactions are being forwarded by this contract
     address signer;
-    // Destination is the address of the contract that method calls are sent to
-    address destination;
-    // Nonce is to keep from having replay attacks
-    uint nonce;
     
-    // Ensures that the signer has signed the hash of the desired method call
-     modifier onlySigner(
-        // methodSignature is sha3("methodName(paramType,paraType...)")
-        bytes32 methodSignature, 
-        bytes32[] callData,
-        uint _nonce,
-        uint8 v, 
-        bytes32 r, 
-        bytes32 s
-    ) {
-        if (_nonce <= nonce) throw;
-        nonce = _nonce;
-        bytes32 hash = sha3(methodSignature, callData, _nonce);
-        if (ecrecover(hash, v, r, s) != signer) throw;
-        _
-    }
-    
-    function SignatureProxy(address _signer, address _destination) {
+    // Nonces are to prevent replay attacks
+    mapping (bytes32 => uint) public hashNonces;
+
+    function SignatureProxy(address _signer) {
         signer = _signer;
-        destination = _destination;
-        nonce = 0;
     }
-    
+
     // Returns true upon success and false upon failure
     function forward(
-        bytes32 methodSignature, 
-        bytes32[] callData,
-        uint8 v, 
-        bytes32 r, 
-        bytes32 s
+        address _destination,
+        // methodSignature is sha3("methodName(paramType,paraType...)")
+        bytes32 _methodSignature,
+        bytes32[] _callData,
+        uint _nonce,
+        bytes32 _hash,
+        uint8 _v,
+        bytes32 _r,
+        bytes32 _s
     )
-    onlySigner(methodSignautre, callData, v, r, s) returns (bool) {
-        return destination.call(bytes4(hash), callData);
+    onlyIfSigned(_hash, _v, _r, _s) returns (bool) {
+      hash = sha3(_destination, _methodSignature, _callData, _nonce);
+      if (_nonce != (hashNonces[hash]] + 1)
+          || hash != _hash) throw;
+      hashNonces[hash] += 1;
+      return destination.call(bytes4(hash), callData);
     }
+
+    // Ensures that the signer has signed the hash of the desired method call
+    modifier onlyIfSigned(
+       bytes32 _hash,
+       uint8 v,
+       bytes32 r,
+       bytes32 s
+   ) {
+       if (ecrecover(_hash, v, r, s) != signer) throw;
+       _
+   }
 }
